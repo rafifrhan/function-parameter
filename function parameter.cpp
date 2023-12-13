a6ad23ec37ca79a262118b6257c74a1cda9fa11f
rafifrhan <114738313+rafifrhan@users.noreply.github.com>
2023-12-13 22:12:11 +0700

    add program to repository

---
+++ function parameter.cpp
@@ -0,0 +1,18 @@
+//Muhammad Rafi Farhan
+//202131145
+#include <iostream>
+using namespace std;
+
+int ganti (int x){
+	x = 23;
+	cout <<"x +"<<x<<endl;
+}
+int main(){
+	int a = 1;
+	cout<<"a = "<a<<endl;
+	ganti(a);
+	cout<<"a = "<<a,,endl;
+	
+	return 0;
+	
+}
